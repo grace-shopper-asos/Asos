1623c8e9edfd7e8157699c44af53f868f88af325
Claire Filipek <cfilipek@sva.edu>
2019-07-29 23:29:43 -0400

    Added nav, styling, and react-bootstrap

--- client/app.js
+++ client/app.js
@@ -2,10 +2,12 @@ import React from 'react'
 
 import {Navbar} from './components'
 import Routes from './routes'
+import {Nav} from './components'
 
 const App = () => {
   return (
     <div>
+      <Nav />
       <Navbar />
       <Routes />
     </div>
--- client/components/Nav.js
+++ client/components/Nav.js
@@ -12,8 +12,8 @@ const Nav = () => {
         className="sticky white-nav"
       >
         <Navbar.Brand>
-          <Link className="nav-hover" to="/">
-            <img src="" alt="logo" />
+          <Link className="nav-hover logo" to="/">
+            TCY
           </Link>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
@@ -22,15 +22,28 @@ const Nav = () => {
           className="justify-content-end"
         >
           <Navbar.Text className="nav-padding-right">
-            <Link className="nav-hover" to="/campuses">
+            <Link className="nav-hover" to="/shop">
               Shop Now
             </Link>
           </Navbar.Text>
           <Navbar.Text className="nav-padding-right">
-            <Link className="nav-hover" to="/students">
+            <Link className="nav-hover" to="/sign-in">
+              Sign Up
+            </Link>
+          </Navbar.Text>
+          <Navbar.Text className="nav-padding-right">
+            <Link className="nav-hover" to="/sign-in">
               Sign In
             </Link>
           </Navbar.Text>
+          <Navbar.Brand>
+            <Link className="nav-hover" to="/sign-in">
+              <img
+                src="https://i.imgur.com/aolKGAl.png"
+                style={{width: 35, marginTop: -7}}
+              />
+            </Link>
+          </Navbar.Brand>
         </Navbar.Collapse>
       </Navbar>
     </div>
