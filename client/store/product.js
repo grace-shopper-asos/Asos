97875c9562a673a711999c7a670f4295449a7287
Teressa <tson1188@gmail.com>
2019-07-30 17:04:28 -0400

    reducer for products setup

---
+++ client/store/product.js
@@ -0,0 +1,58 @@
+import axios from 'axios'
+
+/**
+ * ACTION TYPES
+ */
+const GET_ALL_PRODUCTS = 'GET_ALL_PRODUCTS'
+const GET_SINGLE_PRODUCT = 'GET_SINGLE_PRODUCT'
+
+/**
+ * INITIAL STATE
+ */
+const defaultStore = {products: [], product: {}}
+
+/**
+ * ACTION CREATORS
+ */
+const getProducts = products => ({type: GET_ALL_PRODUCTS, products})
+const getSingleProduct = product => ({type: GET_SINGLE_PRODUCT, product})
+
+/**
+ * THUNK CREATORS
+ */
+export const gotProducts = () => {
+  return async function(dispatch) {
+    try {
+      const res = await axios.get('/api/products')
+      const listofProducts = res.data
+      dispatch(getProducts(listofProducts))
+    } catch (err) {
+      console.log('Something went wrong', err)
+    }
+  }
+}
+
+export const gotOneProduct = id => {
+  return async function(dispatch) {
+    try {
+      const {data} = await axios.get(`/api/students/${id}`)
+      dispatch(getSingleProduct(data))
+    } catch (err) {
+      console.log('Something went wrong', err)
+    }
+  }
+}
+
+/**
+ * REDUCER
+ */
+export default function(state = defaultStore, action) {
+  switch (action.type) {
+    case GET_ALL_PRODUCTS:
+      return action.products
+    case GET_SINGLE_PRODUCT:
+      return action.product
+    default:
+      return state
+  }
+}
