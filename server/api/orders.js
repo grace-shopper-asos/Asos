cd54c59a2c57f6b53b144eef7a9816dec0511d0f
Yulia S <yuliasl@yahoo.com>
2019-07-31 11:57:17 -0400

    order routes in progress

--- server/api/orders.js
+++ server/api/orders.js
@@ -3,4 +3,33 @@ const {Orders, User, Products} = require('../db/models')
 
 module.exports = router
 
-//use router.post to add new order in a db with  user id can be req.sessionID
+//create a new guest order
+router.post('/guest', async (req, res, next) => {
+  try {
+    //use req.sesion.id to determine if there is a session for that user
+    //check if req.session.cart has been created, if so then we call add to cart method on our orders table
+    //otherwise we create an orderId on our table
+    const newOrder = await Orders.create({
+      status: 'open'
+    })
+    res.json(newOrder.id)
+  } catch (err) {
+    next(err)
+  }
+})
+
+// create a new order
+router.post('/:userId', async (req, res, next) => {
+  try {
+    const newOrder = await Orders.create({
+      status: 'open'
+    })
+
+    newOrder.setUser(req.params.userId)
+    res.json(newOrder.id)
+  } catch (err) {
+    next(err)
+  }
+})
+
+///if the order already exists we need a route to add an item to our cart column
--- server/db/models/orders.js
+++ server/db/models/orders.js
@@ -1,5 +1,6 @@
 const Sequelize = require('sequelize')
 const db = require('../db')
+const {Products} = require('./products')
 
 const Orders = db.define('orders', {
   id: {
@@ -11,7 +12,20 @@ const Orders = db.define('orders', {
     type: Sequelize.ENUM('open', 'closed'),
     allowNull: false,
     defaultValue: 'open'
+  },
+  cart: {
+    type: Sequelize.ARRAY(Sequelize.TEXT),
+    defaultValue: []
   }
 })
+
+Orders.prototype.addToCart = function(id) {
+  const addItem = Products.findbyPk(id)
+  this.cart.push(addItem)
+}
+Orders.prototype.deleteFromCart = function(id) {
+  const deleteItem = Products.findbyPk(id)
+  //remove item from cart array
+}
 
 module.exports = Orders
